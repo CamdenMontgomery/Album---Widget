b32d57cbe2453e4969bb0d83d29cc16c2cde6666
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-24 13:50:53 -0400

    feat: add rounded corners to toolbar

--- src/view/windows/Widget.py
+++ src/view/windows/Widget.py
@@ -11,12 +11,14 @@ class Widget(QtWidgets.QMainWindow):
 
         self.setWindowTitle("Album")
         self.resize(800, 24)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
         self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.WindowStaysOnTopHint)
         self.setObjectName("MainWindow")
         
         toolbar = QtWidgets.QToolBar("Tools")
         toolbar.setIconSize(QtCore.QSize(24, 24))
         toolbar.setMovable(False)
+        toolbar.setObjectName('Toolbar')
         self.addToolBar(toolbar)
         
         self.folder_add_button = ANewFolder()
