1be2a11f3e2bab3b74e1e75a78e1018ffde1bf38
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-28 13:04:39 -0400

    feat: add formatting to folder selector and remove '/' from workspace button

--- src/view/components/FolderSelector.py
+++ src/view/components/FolderSelector.py
@@ -33,19 +33,29 @@ class FolderSelector(QtWidgets.QComboBox, UseStore):
         
     def on_state_changed(self):
         
-        #Update selection if current foler changed
-        if self.currentText() == self.store_.state["current_folder_name"]: return
-        index = self.findText(self.store_.state["current_folder_name"])
-        if index > -1: self.setCurrentIndex(index )
+        #Text in the selector will be in this format so to compare the text in th selector we must format what were comparing it to
+        def format(text): return './' + text.capitalize()
         
         #Only update if workspace changed to avoid self.clear calling on_folder_changed and resetting the selection for every update to the state
-        if self.workspace == self.store_.state["workspace_dir"]: return
+        if self.workspace != self.store_.state["workspace_dir"]:
+            #Populate selector with workspace folders
+            self.clear()
+            for folder in self.store_.state["workspace_folders"]:
+                self.addItem(format(folder))
         self.workspace = self.store_.state["workspace_dir"]
         
-        #Populate selector with workspace folders
-        self.clear()
-        for folder in self.store_.state["workspace_folders"]:
-            self.addItem(folder)
+
+        
+        #Update selection if current foler changed
+        current_folder = self.store_.state["current_folder_name"]
+        
+        if current_folder != None and self.currentText() != format(current_folder):       
+            index = self.findText(format(current_folder))
+            if index > -1: self.setCurrentIndex(index)
+        
+
+        
+
            
         
     
